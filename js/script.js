4ed03e4b524c0a7371c321343f1abe640f221547
Paulo <trigopaulo@gmail.com>
2022-08-22 23:52:11 -0300

    mobile version added

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ let buttonElement = document.querySelector('#button');
 let sizePassword = document.querySelector('#value');
 let password = document.querySelector('#password');
 
-let containerPassword = document.querySelector("container-input");
+let containerPassword = document.querySelector('#container-password');
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$%&<>"
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$%&"
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
@@ -23,11 +23,13 @@ function generatePassword(){
     let pass = "";
     //generate password from password length
     for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-        pass += charset.charAt(Math.floor(Math.random()*n));
+        pass += charset.charAt(Math.floor(Math.random() * n));
     }
+
     //removing class "hide"
     containerPassword.classList.remove("hide");
-    console.log(pass);
+    password.innerHTML = pass;
+    novaSenha = pass;
 }
 
 //copying the password
